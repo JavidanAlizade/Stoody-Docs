1aacbf71abf91c896c7ec6c8dda11b0838b9c3c8
JavidanAlizade <cavidanalizade16@gmail.com>
2025-02-21 01:06:51 +0400

    Fixed Ui: Corrected default texts and images, deleted guidance part on main menu, gived glow to Stoody title on main menu, dark theme turned into default theme.

--- stoody-internal-doc/src/pages/index.js
+++ stoody-internal-doc/src/pages/index.js
@@ -12,10 +12,10 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <Heading as="h1" className="hero__title">
+          <Heading as="h1" className="hero__title" style={{ color: "#4FDDBF", textShadow: "0 0 3px #4FDDBF, 0 0 20px #4FDDBF, 0 0 30px #4FDDBF"  }}>
           {siteConfig.title}
         </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <p className="hero__subtitle" style={{ color: "white" }}>{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
